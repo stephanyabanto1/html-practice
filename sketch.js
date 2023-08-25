d1ad68630b8fc6b6537510af8a08ffca551d021e
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-25 19:46:33 -0400

    dsff

--- sketch.js
+++ sketch.js
@@ -1,33 +1,12 @@
-const canvas = document.getElementById("myCanvas");
-
-
-const ctx = canvas.getContext("2d");
-
-// ctx.moveTo(0,0);
-// ctx.lineTo(200,100);
-// ctx.stroke();
-
-
-// ctx.beginPath();
-// ctx.arc(50,50, 10, 0, (2/Math.PI));
-// ctx.stroke();
-
-
-
-let x = 0;
-
-
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(x,x, 10, 0, Math.PI*2);
-
-    ctx.stroke();
-    x += 0.1;
-
-}
-
-animate();
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
