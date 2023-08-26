bf0463168476b39fa4509689fd4fd26d78caf6f7
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-08-25 20:49:32 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,23 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400, WEBGL);
   }
   
-  function draw() {
-    if (mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(255);
-    }
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+//   function draw() {
+//     if (mouseIsPressed) {
+//       fill(0);
+//     } else {
+//       fill(255);
+//     }
+//     ellipse(mouseX, mouseY, 80, 80);
+//   }
+
+
+function draw(){
+    background(220);
+    rotateX(millis()/1000);
+    rotateY(millis()/1000);
+    box();
+    sphere(100);
+}
+
+
