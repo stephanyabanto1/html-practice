e6a8b8f55ce5ff33b5dbb292d959a9fd21b6b62e
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-09-05 21:23:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,65 @@
-function setup() {
-    createCanvas(400, 600, WEBGL);
-    print("Hello");
-    background(0);
-  }
-  
-//   function draw() {
-//     if (mouseIsPressed) {
-//       fill(0);
-//     } else {
-//       fill(255);
-//     }
-//     ellipse(mouseX, mouseY, 80, 80);
-//   }
+var ball1 = {
 
+  x : 200,
+  y : 300,
+  xspeed :4,
+  yspeed: 4
+
+}
+var ball2 = {
+  x:400,
+  y:600,
+  xspeed: 4,
+  yspeed: 4,
+}
+
+function setup(){
+
+  createCanvas(800, 800);
+}
 
 function draw(){
-    
-  
-  
-    
-    noStroke();
-    fill("pink");
-    // box();
-    fill(255);
-    ellipse(mouseX, mmouseY, 24);
+  background(0);
+  stroke(225);
+  strokeWeight(5);
+  noFill();
+  ellipse(ball1.x, ball1.y, 24, 24)
+  ellipse(ball2.x, ball2.y, 30,30)
+
+
+
+  ballMove(ball1);
+  ballMove(ball2);
+
+
 }
+
+function ballMove(ball){
+
+  ball.x = ball.x + ball.xspeed;
+  ball.y = ball.y - ball.yspeed;
+
+  if(ball1.y<0){
+    ball1.yspeed = ball1.yspeed + 2;
+    ball1.y = 800;
+    ball1.x = 300;
+
+  }
+  if(ball2.y<0){
+    ball2.y = 800;
+    ball2.x = 200;
+
+  }
+
+
+}
+
+
+
+ 
+
+ 
+
+
 
 
