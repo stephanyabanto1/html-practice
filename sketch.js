d5ae56d05a2dc74dc660dbc8a760f76babb71795
Stephany Abanto <63083633+stephanyabanto1@users.noreply.github.com>
2023-10-01 16:25:21 -0400

    party1

--- sketch.js
+++ sketch.js
@@ -1,27 +1,46 @@
 
-Segment seg;
+let x2=250; //We want this one to move left and right 
+let y2=250;
+let y1=0;
+let x1 =200;
+let directionR = true;
 
-class Segment{
-
-  PVectora;
-  PVectorb ;
-}
-
-
-
-
+let length;
 
 function setup(){
 createCanvas(400,400);
-seg = new Segment(300,200,100,0);
-  
+ 
 
 }
 
 
 function draw(){
 background(153);
-  
+
+stroke('blue');
+line(200,0,200,250);
+
+
+
+line(x1,y1,x2,y2);
+
+x2= x2+2;
+if(x2>400)
+{
+  x2=width;
+  directionR = false;
+}
+
+
+stroke('black');
+
+
+
+
+
+
+length = (y2-x2)/(x2-x1);
+
 
  
 }
